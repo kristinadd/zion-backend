ea6030a90fce006d6e47341d867ce425066b7c1d
kristinadd <kristina@productboard.com>
2025-11-26 21:52:48 +0200

    e p

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # Enable server timing.
   config.server_timing = true
 
+  # Allow Envoy health checks (cluster name as Host header)
+  config.hosts << "ledger_service_ingress"
+
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
